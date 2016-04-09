b303f65f370567f9ad827e5e70435fba438658fc
Oskari Noppa <the.nobba@gmail.com>
2016-04-09 19:43:36 +0300

    Successfully read survey a template

--- form/server.js
+++ form/server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     path	= require('path'),
+	db    = require('../db'),
+	fs = require('fs'),
     router	= express.Router();
 
 
@@ -11,7 +13,37 @@ router
 
 router
     .get('/:id', (req, res) => {
-	    res.send('hahaa ' + req.id);
+
+	    db.select('*')
+		    .from('surveys')
+		    .where({ id: req.id })
+		    .then(function(rows) {
+				if(!rows || rows.length === 0) {
+					notFound(res);
+				} else {
+
+					//The file will be located at templates/compiled/<unique filename>
+					var file = path.join(__dirname, 'templates', 'compiled', rows[0].template + '.html');
+
+					fs.stat(file, function(err, stat){
+						if(err == null) {
+							res.writeHead(200, {
+								'Content-Type': 'text/html',
+								'Content-Length': stat.size
+							});
+
+							fs.createReadStream(file).pipe(res);
+						} else {
+							notFound(res);//haa
+						}
+					});
+				}
+		    });
+
     });
+
+function notFound(res){
+	res.status(404).send('Not found');
+}
 
 module.exports = router;
