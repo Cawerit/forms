54560980741d151e52699d962240f2c59373bf32
Oskari Noppa <the.nobba@gmail.com>
2016-05-02 02:38:51 +0300

    Separate credentials in json files
    Credentials are no longer kept in the logic, so db.js can be added to source
    control. Please note that it is now necessary to  acquire the "credentials" folder
    with its contents for the project to work. The credentials folder is not included in
    source control for security reasons.

---
+++ db.js
@@ -0,0 +1,11 @@
+var knex = require('knex');
+
+// Load the file containing the connection configuration.
+// NOTE: For security reasons, this file is not included in the source control.
+// Request the file from the author of this project and drop it in folder "credentials" to get access to db.
+var config = require('./credentials/rds.credentials.json');
+
+module.exports = knex({
+	client: 'mysql',
+	connection: config
+});
